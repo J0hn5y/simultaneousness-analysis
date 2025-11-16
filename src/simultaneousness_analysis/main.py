ab4d2c07574cb6d071985704740bbbc6a5b53e75
JohnSilent <36659558+JohnSilent@users.noreply.github.com>
2025-11-16 13:01:30 +0100

    data import executable from main.py and add a flag to enable/disable it.

--- src/simultaneousness_analysis/main.py
+++ src/simultaneousness_analysis/main.py
@@ -1,7 +1,7 @@
 from .retrieve_data_from_dwd_cdc import main as retrieve_data
 
 # Set True to retrieve data from DWD CDC for station in S-H
-RETRIEVE_DATA: bool = False
+RETRIEVE_DATA: bool = True
 
 
 def main() -> None:
