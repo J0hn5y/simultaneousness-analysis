252f16df67fee2d7f9bd5250accd24b1d5fde467
J0hn5y <J0hn5y@gentlemansclub.de>
2026-02-13 15:30:26 +0100

    simplify data request

--- src/simultaneousness_analysis/data_request.py
+++ src/simultaneousness_analysis/data_request.py
@@ -4,32 +4,33 @@ import re
 import pandas as pd
 from pandas.api.types import CategoricalDtype
 
-# from word2number import w2n # why cant this module be found?
+# from word2number import w2n  # why cant this module be found?
 
 # 1. function: provide dataframe of all products with their attributes parsed from file names
 
 
-def generate_meta_search_table(DATA_PATH) -> pd.DataFrame:
-    """Generates a table (dataframe) to search data paths by meta attributes!
+def generate_meta_search_table(data_path: Path) -> pd.DataFrame:
+    """Seg!
 
-    :param DATA_PATH: Pathlib object which points on folder with provided data files
-    :return: DataFrame with path object as index and meta attributes as columns
-    :rtype: DataFrame
+    Args:
+        data_path (Path): _description_
+
+    Returns:
+        pd.DataFrame: _description_
     """
-    # fetch all file names and connect them to stations
-    file_paths = list(DATA_PATH.glob("**/*.txt"))
+    # fetch all file names anmd store thwm in a list
+    file_paths = list(data_path.glob("**/*.txt"))
     # metadata are excluded because they are json files
     file_names = [fp.name for fp in file_paths]
-    file_amount = len(file_names)
-    print(f"Found {file_amount} data files.")
+    # TODO usefp.stem und .suffixto avoid using re
 
     # split file name on every "_" and every "." to gain all intel from the name
     product_list = [re.split(r"[_.]", fn) for fn in file_names]
     # path/path/example produkt_zehn_min_tu_19910417_19991231_04466.txt
 
     # add file_path as last element to each product entry
-    for i in range(file_amount):
-        product_list[i].append(file_paths[i])
+    for i, file_path in enumerate(file_paths):
+        product_list[i].append(file_path)
     # all appended file paths should be path objects
 
     product_kwargs = {
@@ -51,12 +52,11 @@ def generate_meta_search_table(DATA_PATH) -> pd.DataFrame:
     product_df["to_date"] = pd.to_datetime(product_df["to_date"], format="%Y%m%d")
     product_df["stations_id"] = product_df["stations_id"].astype(int)
     # TODO parse resolution_value as integer with package "word2number" because the values are written as words in german
+    # resolution_value_list = []
+    # for value in product_df["resolution_value"]:
+    #    resolution_value_list.append(w2n.word_to_num(value))
+    # product_df["resolution_value"] = resolution_value_list
 
-    # translate measurand to full name (categorical) sd --> solar radiation,...
-    measurand_type = CategoricalDtype(categories=["sd", "tu", "ff"])
-    measurand_name_type = CategoricalDtype(
-        categories=["solar radiation", "air temperature", "wind speed"],
-    )
     # translate measurand codes to full names
     measurand_mapping = {
         "sd": "solar radiation",
@@ -64,9 +64,9 @@ def generate_meta_search_table(DATA_PATH) -> pd.DataFrame:
         "ff": "wind speed",
     }
     product_df["measurand_names"] = (
-        product_df["measurand"].map(measurand_mapping).astype(measurand_name_type)
+        product_df["measurand"].map(measurand_mapping).astype("category")
     )
-    product_df["measurand"] = product_df["measurand"].astype(measurand_type)
+    product_df["measurand"] = product_df["measurand"].astype("category")
     # all other are strings and can be stored as categorical too
     product_df["type"] = product_df["type"].astype("category")
     product_df["resolution_unit"] = product_df["resolution_unit"].astype("category")
@@ -74,17 +74,16 @@ def generate_meta_search_table(DATA_PATH) -> pd.DataFrame:
 
     # 2. join metadata
     # Metadata file paths (constants)
-    META_DATA_TEMPERATURE = DATA_PATH / "station_metadata_temperature.json"
-    META_DATA_SOLAR = DATA_PATH / "station_metadata_solar.json"
-    META_DATA_WIND = DATA_PATH / "station_metadata_wind.json"
+    META_DATA_TEMPERATURE = data_path / "station_metadata_temperature.json"
+    META_DATA_SOLAR = data_path / "station_metadata_solar.json"
+    META_DATA_WIND = data_path / "station_metadata_wind.json"
     print(META_DATA_TEMPERATURE)
     # station metadata (attributes that are independent from measurements)
     # Remove date range columns as they depend on the measurements
     # and remove abgabe as it is not relevant for this use case
     metadata_temperature = pd.read_json(META_DATA_TEMPERATURE)
-    metadata_temperature.drop(
+    metadata_temperature = metadata_temperature.drop(
         columns=["von_datum", "bis_datum", "Abgabe"],
-        inplace=True,
     )
     print(len(metadata_temperature))
     metadata_temperature.set_index(["Stations_id"], inplace=True)
@@ -129,13 +128,14 @@ def generate_meta_search_table(DATA_PATH) -> pd.DataFrame:
 
 def search_paths_by_meta_data(
     search_table: pd.DataFrame,
-    measurand_names=[],
-    from_date=None,
-    to_date=None,
-    station_ids=[],
-    station_names=[],
-    federal_states=[],
+    measurand_names: list | None = None,
+    from_date: pd.Timestamp | None = None,
+    to_date: pd.Timestamp | None = None,
+    station_ids: list[int] | None = None,
+    station_names: list[str] | None = None,
+    federal_states: list[str] | None = None,
 ) -> list:
+    # use default factory or set default to None or empty list to avoid mutable default arguments
     """Search paths by meta data from search table!
 
     Args:
@@ -152,28 +152,25 @@ def search_paths_by_meta_data(
 
     """
     # 3. function: "simple" filter dataframe by measurand, date range, station id and return list of paths
+    filtered = search_table.copy()
 
-    measurand_names_filter_var = (
-        measurand_names if measurand_names != [] else search_table["measurand_names"]
-    )
+    if measurand_names is not None:
+        filtered = filtered[filtered["measurand_names"].isin(measurand_names)]
 
-    from_date_filter_var = (
-        from_date if from_date is not None else search_table["from_date"]
-    )
+    if from_date is not None:
+        filtered = filtered[filtered["from_date"] >= from_date]
 
-    to_date_filter_var = to_date if to_date is not None else search_table["to_date"]
+    if to_date is not None:
+        filtered = filtered[filtered["to_date"] <= to_date]
 
-    station_id_filter_var = (
-        station_ids if station_ids != [] else search_table["stations_id"]
-    )
+    if station_ids is not None:
+        filtered = filtered[filtered["stations_id"].isin(station_ids)]
 
-    station_name_filter_var = (
-        station_names if station_names != [] else search_table["station_name"]
-    )
+    if station_names is not None:
+        filtered = filtered[filtered["station_name"].isin(station_names)]
 
-    federal_state_filter_var = (
-        federal_states if federal_states != [] else search_table["federal_state"]
-    )
+    if federal_states is not None:
+        filtered = filtered[filtered["federal_state"].isin(federal_states)]
 
     # possible filter parameters:
     # measurand ("code")
@@ -182,16 +179,6 @@ def search_paths_by_meta_data(
     # type
     # Area (altitude, latitude, longitude and PLZ,.. after merging with additional metadata)
 
-    filtered = search_table[
-        (search_table["measurand_names"].isin(measurand_names_filter_var))
-        & (
-            search_table["to_date"] >= from_date_filter_var
-        )  # crossover of from and to is correct !
-        & (
-            search_table["from_date"] <= to_date_filter_var
-        )  # crossover of from and to is correct !
-        & (search_table["stations_id"].isin(station_id_filter_var))
-    ]
     path_list = filtered.index.tolist()
     return path_list
 
@@ -201,12 +188,14 @@ def main() -> None:
     DATA_PATH = Path.cwd() / "data" / "cdc" / "raw"
 
     meta_search_table = generate_meta_search_table(DATA_PATH)
+    # to cache meta data search tabele it es nessassary to use relative paths with working directory as root
+
     # use request-class?
     # search paths by meta data
     result = search_paths_by_meta_data(
         search_table=meta_search_table,
         measurand_names=["solar radiation"],
-        station_ids=[1200],
+        station_ids=[1200, 2961],
     )
     print(result)
     # next step is to convert paths into dataframes with the real data
